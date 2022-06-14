eb524e05cf27fb6806a5a25c82aa290c13930a34
A.C.E07 <73689800+ACE07-Sev@users.noreply.github.com>
2022-06-14 17:07:57 +0800

    Update CQM_TSP.py
    Added the subtour elimination constraint

--- CQM_TSP.py
+++ CQM_TSP.py
@@ -8,7 +8,24 @@ os.chdir('..')
 from dimod import ConstrainedQuadraticModel, Binary, quicksum
 from dwave.system import LeapHybridCQMSampler
 import numpy as np
+import itertools
 from matplotlib import pyplot as plt
+
+
+# Function to generate the subsets of a list given size
+def findsubsets(s, n):
+    return list(itertools.combinations(s, n))
+
+
+# Function to generate all the subsets of a list
+def find_all_subsets(s):
+    subtours = []
+    temp_list = []
+    for i in range(2, len(s)-1):
+        temp_list = findsubsets(s, i)
+        for j in range(len(temp_list)):
+            subtours.append(temp_list[j])
+    return subtours
 
 
 # Function for calculating the euclidean distance
@@ -43,6 +60,12 @@ C_ = [[0, 2, 2, 4, 2],
       [4, 2, 3, 0, 3],
       [2, 2, 2, 3, 0]]
 
+C_1 = [[0.0, 2.23606797749979, 2.23606797749979, 3.1622776601683795, 4.0],
+       [2.23606797749979, 0.0, 1.4142135623730951, 1.0, 2.23606797749979],
+       [2.23606797749979, 1.4142135623730951, 0.0, 2.23606797749979, 3.605551275463989],
+       [3.1622776601683795, 1.0, 2.23606797749979, 0.0, 1.4142135623730951],
+       [4.0, 2.23606797749979, 3.605551275463989, 1.4142135623730951, 0.0]]
+
 # Initializing the CQM
 cqm = ConstrainedQuadraticModel()
 # Initializing the objective
@@ -50,30 +73,37 @@ objective = 0
 # Initializing the constraints
 constraint_1 = 0
 constraint_2 = 0
+constraint_3 = 0
 # number of nodes
 n = 5
-# Distance Matrix
-C_in = []
-temp_set = []
-coordinates = []
-# Inputting the coordinates
-for i in range(n):
-    print("Input x coordinate")
-    point_x = float(input())
-    print("Input y coordinate")
-    point_y = float(input())
-    coordinates.append([point_x, point_y])
-    plt.scatter(point_x, point_y)
-# Dot plot
-plt.show()
-# Generating the distance matrix
-for i in range(n):
-    for j in range(n):
-        temp_set.append(distance_between_points(coordinates[i], coordinates[j]))
-    C_in.append(temp_set)
-    temp_set = []
-# Distnace Matrix
-print(C_in)
+global_set = [1, 2, 3, 4, 5]
+# list of subtours
+subtours = find_all_subsets(global_set)
+print(subtours)
+# number of subsets
+S = len(subtours)
+# # Distance Matrix
+# C_in = []
+# temp_set = []
+# coordinates = []
+# # Inputting the coordinates
+# for i in range(n):
+#     print("Input x coordinate")
+#     point_x = float(input())
+#     print("Input y coordinate")
+#     point_y = float(input())
+#     coordinates.append([point_x, point_y])
+#     plt.scatter(point_x, point_y)
+# # Dot plot
+# plt.show()
+# # Generating the distance matrix
+# for i in range(n):
+#     for j in range(n):
+#         temp_set.append(distance_between_points(coordinates[i], coordinates[j]))
+#     C_in.append(temp_set)
+#     temp_set = []
+# # Distnace Matrix
+# print(C_in)
 
 # Initializing the decision var
 X_ = []
@@ -85,10 +115,10 @@ for i in range(n):
             X_.append(0)
         else:
             X_.append(Binary('X_' + str(i + 1) + "_" + str(j + 1)))
-    objective += quicksum(C_in[j][i] * X_[j] for j in range(n))
+    objective += quicksum(C_1[j][i] * X_[j] for j in range(n))
     X_.clear()
 
-# Constraints
+# Assignment Constraints (Only one entry and exit per node)
 for i in range(n):
     for j in range(n):
         if i == j:
@@ -109,9 +139,21 @@ for j in range(n):
     cqm.add_constraint(constraint_2 == 1, label="Constraint 2-" + str(j + 1))
     X_.clear()
 
+# subtour elimination constraint
+for s in range(S):
+    for i in range(len(subtours[s])):
+        for j in range(len(subtours[s])):
+            if i == j:
+                X_.append(0)
+            else:
+                X_.append(Binary('X_' + str(i + 1) + "_" + str(j + 1)))
+        constraint_3 = quicksum(X_[j] for j in range(len(subtours[s])))
+        cqm.add_constraint(constraint_3 <= len(subtours[s]) - 1, label="Constraint 3-" + str(i + 1) + str(s + 1))
+        X_.clear()
+
 # Running the sampler to get the sample set
 cqm_sampler = LeapHybridCQMSampler()
-sampleset = cqm_sampler.sample_cqm(cqm, label='CQMAmirali', time_limit=12)
+sampleset = cqm_sampler.sample_cqm(cqm, label='CQMAmirali', time_limit=5)
 
 # Printing the sample set
 for c, cval in cqm.constraints.items():
@@ -121,7 +163,9 @@ feasible_sampleset = sampleset.filter(lambda d: d.is_feasible)
 
 sample = feasible_sampleset.first.sample
 Formatter(width=1000).fprint(feasible_sampleset)
-
+# print('')
+# print('')
+# Formatter(width=1000).fprint(sampleset)
 # print(feasible_sampleset)
 
 for constraint in cqm.iter_constraint_data(sample):
