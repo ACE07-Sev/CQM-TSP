190da14e3a19d73e6dab4d33ce14147017c42d80
A.C.E07 <73689800+ACE07-Sev@users.noreply.github.com>
2022-06-14 15:11:04 +0800

    Create CQM_TSP.py

---
+++ CQM_TSP.py
@@ -0,0 +1,134 @@
+import math
+import os
+
+import geopy
+from dimod.serialization.format import Formatter
+
+os.chdir('..')
+from dimod import ConstrainedQuadraticModel, Binary, quicksum
+from dwave.system import LeapHybridCQMSampler
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# Function for calculating the euclidean distance
+def distance_between_points(point_A, point_B):
+    return np.sqrt((point_A[0] - point_B[0]) ** 2 + (point_A[1] - point_B[1]) ** 2)
+
+
+# Function for calculating the distance using coordinates
+def distance_two_coordinates(point_A, point_B):
+    lat1 = math.pi * ((point_A[0] // 1) + 5.0 * (point_A[0] % 1) / 3.0) / 180.0
+    lon1 = math.pi * ((point_A[1] // 1) + 5.0 * (point_A[1] % 1) / 3.0) / 180.0
+    lat2 = math.pi * ((point_B[0] // 1) + 5.0 * (point_B[0] % 1) / 3.0) / 180.0
+    lon2 = math.pi * ((point_B[1] // 1) + 5.0 * (point_B[1] % 1) / 3.0) / 180.0
+
+    q1 = math.cos(lon1 - lon2)
+    q2 = math.cos(lat1 - lat2)
+    q3 = math.cos(lat1 + lat2)
+    radius = 6378.388
+
+    distance = radius * math.acos(1 / 2 * ((1 + q1) * q2 - (1 - q1) * q3)) + 1
+
+    if distance == 1.0:
+        return 0
+
+    return round(geopy.distance.great_circle(point_A, point_B).km)
+
+
+# dataset for distance matrix
+C_ = [[0, 2, 2, 4, 2],
+      [2, 0, 2, 2, 3],
+      [2, 2, 0, 3, 2],
+      [4, 2, 3, 0, 3],
+      [2, 2, 2, 3, 0]]
+
+# Initializing the CQM
+cqm = ConstrainedQuadraticModel()
+# Initializing the objective
+objective = 0
+# Initializing the constraints
+constraint_1 = 0
+constraint_2 = 0
+# number of nodes
+n = 5
+# Distance Matrix
+C_in = []
+temp_set = []
+coordinates = []
+# Inputting the coordinates
+for i in range(n):
+    print("Input x coordinate")
+    point_x = float(input())
+    print("Input y coordinate")
+    point_y = float(input())
+    coordinates.append([point_x, point_y])
+    plt.scatter(point_x, point_y)
+# Dot plot
+plt.show()
+# Generating the distance matrix
+for i in range(n):
+    for j in range(n):
+        temp_set.append(distance_between_points(coordinates[i], coordinates[j]))
+    C_in.append(temp_set)
+    temp_set = []
+# Distnace Matrix
+print(C_in)
+
+# Initializing the decision var
+X_ = []
+
+# Objective
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            X_.append(0)
+        else:
+            X_.append(Binary('X_' + str(i + 1) + "_" + str(j + 1)))
+    objective += quicksum(C_in[j][i] * X_[j] for j in range(n))
+    X_.clear()
+
+# Constraints
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            X_.append(0)
+        else:
+            X_.append(Binary('X_' + str(i + 1) + "_" + str(j + 1)))
+    constraint_1 = quicksum(X_[j] for j in range(n))
+    cqm.add_constraint(constraint_1 == 1, label="Constraint 1-" + str(i + 1))
+    X_.clear()
+
+for j in range(n):
+    for i in range(n):
+        if i == j:
+            X_.append(0)
+        else:
+            X_.append(Binary('X_' + str(i + 1) + "_" + str(j + 1)))
+    constraint_2 = quicksum(X_[i] for i in range(n))
+    cqm.add_constraint(constraint_2 == 1, label="Constraint 2-" + str(j + 1))
+    X_.clear()
+
+# Running the sampler to get the sample set
+cqm_sampler = LeapHybridCQMSampler()
+sampleset = cqm_sampler.sample_cqm(cqm, label='CQMAmirali', time_limit=12)
+
+# Printing the sample set
+for c, cval in cqm.constraints.items():
+    print(c, cval)
+
+feasible_sampleset = sampleset.filter(lambda d: d.is_feasible)
+
+sample = feasible_sampleset.first.sample
+Formatter(width=1000).fprint(feasible_sampleset)
+
+# print(feasible_sampleset)
+
+for constraint in cqm.iter_constraint_data(sample):
+    print(constraint.label, constraint.violation)
+
+for c, v in cqm.constraints.items():
+    print('lhs : ' + str(v.lhs.energy(sample)))
+    print('rhs : ' + str(v.rhs))
+    print('sense  : ' + str(v.sense))
+    print("---")
